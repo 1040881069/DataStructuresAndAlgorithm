e401e8c0c80f1a654f33eb45f536fc6a84a3f5dc
1040881069 <58197581+1040881069@users.noreply.github.com>
2019-12-31 09:05:14 +0800

    人工智能

---
+++ 人工智能/Enumera/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using static System.Net.WebRequestMethods;
+
+namespace Enumera
+{
+    public  class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var Kestring = "人工智能";
+            TestReadingFile(Kestring);
+            Console.WriteLine("叶公子高好龙，钩以写龙，凿以写龙，屋室雕文以写龙。于是天龙闻而下之，窥头于牖，施尾于堂。 叶公见之，弃而还走，失其魂魄，五色无主。是叶公非好龙也，好夫似龙而非龙者也。");
+            TestStreamReaderEnumerable(Kestring);
+            Console.ReadKey();
+
+            Console.WriteLine("Hello World!");
+            Console.Read();
+        }
+
+        private static void TestStreamReaderEnumerable(string kestring)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private static void TestReadingFile(string kestring)
+        {
+            Console.WriteLine("数量");
+            //throw new NotImplementedException();
+        }
+
+       
+    }
+}
